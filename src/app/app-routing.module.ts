46e39f83259958fc4590bc79c7c32f57a08ebd55
maria de los angeles <mariaramireeez2002@gmail.com>
2022-05-03 21:37:12 -0400

    header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { UserComponent } from './user/user.component'
 const routes: Routes = [
   {
     path:'user',
-  component:UserComponent
+  component: UserComponent
   },
   
 ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import {ExtendedModule, FlexModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
+    HeaderComponent,
     
   ],
-  imports: [
-    AppRoutingModule,
-    BrowserModule
-  ],
+    imports: [
+        AppRoutingModule,
+        BrowserModule,
+        FlexModule,
+        ExtendedModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
