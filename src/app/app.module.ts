1895e6483fef96c8a4766fc7f3fbc9337a9f91d0
maria de los angeles <mariaramireeez2002@gmail.com>
2022-05-06 23:48:04 -0400

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,8 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import {ExtendedModule, FlexModule} from "@angular/flex-layout";
-<<<<<<< HEAD
-import {HttpClientModule} from "@angular/common/http";
-=======
+
 import { HttpClientModule } from '@angular/common/http';
->>>>>>> d665ca3a4425351746ddd88f5bf772b4ec05d575
 
 @NgModule({
   declarations: [
