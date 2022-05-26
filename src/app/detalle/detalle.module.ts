95ce6555d17378c0056b55fed21873be43aa0ebe
diego <diegoezc@gmail.com>
2022-05-26 13:59:15 -0400

    hice el menu modal

---
+++ src/app/detalle/detalle.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DetalleComponent } from './detalle.component';
+
+
+
+@NgModule({
+  declarations: [DetalleComponent],
+  imports: [
+    CommonModule,
+    
+  ]
+})
+export class DetalleModule { }
